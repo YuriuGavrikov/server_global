6001989e26d09c89278de032fd7071ec114f13c2
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-09-17 15:47:52 +0300

    test3

--- websocket.js
+++ websocket.js
@@ -3,32 +3,13 @@ import WebSocket, { WebSocketServer } from "ws";
 export function WebSocketFu() {
 	const wss = new WebSocketServer(
 		{
-			port: 6000,
-			perMessageDeflate: {
-				zlibDeflateOptions: {
-					// See zlib defaults.
-					chunkSize: 1024,
-					memLevel: 7,
-					level: 3,
-				},
-				zlibInflateOptions: {
-					chunkSize: 10 * 1024,
-				},
-				// Other options settable:
-				clientNoContextTakeover: true, // Defaults to negotiated value.
-				serverNoContextTakeover: true, // Defaults to negotiated value.
-				serverMaxWindowBits: 10, // Defaults to negotiated value.
-				// Below options specified as default values.
-				concurrencyLimit: 10, // Limits zlib concurrency for perf.
-				threshold: 1024, // Size (in bytes) below which messages
-				// should not be compressed if context takeover is disabled.
-			},
+			port: 8080,
 		},
-		() => console.log(`Server started on 6000`)
+		() => console.log(`Server started on 8080`)
 	);
 
-	wss.on("connection", function connection(WebSocket) {
-		WebSocket.on("message", function (message) {
+	wss.on("connection", function connection(ws) {
+		ws.on("message", function (message) {
 			message = JSON.parse(message);
 			switch (message.event) {
 				case "message":
