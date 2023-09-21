06e5cce32fdf884178d11091801c43900591d619
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-09-21 18:11:33 +0300

    test

--- socketIO.js
+++ socketIO.js
@@ -1,30 +1,9 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-import cors from "cors";
 
 export function WebSocketFu() {
-	const router = express.Router();
-	router.get("/", (req, res) => {
-		res.setHeader("Access-Control-Allow-Origin", "*");
-
-		res.setHeader(
-			"Access-Control-Allow-Methods",
-			"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-		);
-
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			"X-Requested-With,content-type"
-		);
-
-		res.send("Это только мой мир.");
-	});
-
 	const app = express();
-	app.use(cors({ origin: "*" }));
-	app.use(router);
-
 	const server = createServer(app);
 
 	const io = new Server(server, {
