0ce9354e28808a5961bba35f884abb2207ed7348
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-09-22 16:58:05 +0300

    test

--- socketIO.js
+++ socketIO.js
@@ -1,20 +1,24 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+// import cors from "cors";
+
+const PORT = 3000;
 
 export function WebSocketFu() {
 	const app = express();
 	const server = createServer(app);
-
 	const io = new Server(server, {
 		cors: {
-			origin: [
-				"https://yuriugavrikov.github.io/Chat-vue-socketio/",
-				"https://yuriugavrikov.github.io",
-			],
-			methods: ["GET", "POST"],
+			origin: "*",
 		},
 	});
+
+	// app.use(
+	// 	cors({
+	// 		origin: "http://localhost:5173",
+	// 	})
+	// );
 
 	io.on("connection", (socket) => {
 		console.log("connection");
@@ -22,9 +26,12 @@ export function WebSocketFu() {
 			console.log("message");
 			io.emit("message", msg);
 		});
+		socket.on("disconnect", (reason) => {
+			console.log("disconnect", reason);
+		});
 	});
 
-	server.listen(3040, () => {
-		console.log("server running at port 3040");
+	server.listen(PORT, (error) => {
+		error ? console.log(error) : console.log(`listening port ${PORT}`);
 	});
 }
